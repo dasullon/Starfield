c86ee450e51986372f1ec05a56bcfe1bbeaff0e3
unknown <dasullon@697-334-Linhir.sf.k12.internal>
2014-10-20 12:25:59 -0700

    Added first name to index.html

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 
 Particle [] ha_mez = new Particle[100];
 
-void setup()
+public void setup()
 {
 	size(500,500);
 	for(int i = 0; i < ha_mez.length; i++){
@@ -9,7 +25,7 @@ void setup()
 	}
 	ha_mez[0]= new OddballParticle();
 }
-void draw()
+public void draw()
 {
 	translate (250,250);
 	fill(0,0,0,10);
@@ -34,14 +50,14 @@ class NormalParticle implements Particle
 		value2= ((int)((Math.random()*255)+1));
 		value3= ((int)((Math.random()*255)+1));
 	}
-	void show(){
+	public void show(){
 		noStroke();
-		color c = color (value1, value2, value3);		
+		int c = color (value1, value2, value3);		
 		fill(c);
 		ellipse((float)x,(float)y,15,15);
 		
 	}
-	void move(){
+	public void move(){
 		x+= Math.cos(dTheta)*dSpeed;
 		y+= Math.sin(dTheta)*dSpeed;
 		if((x>250||y>250 || x<-250 || y<-250)){
@@ -68,15 +84,24 @@ class OddballParticle  implements Particle
 		dSpeed= Math.random()*3;
 
 	}	
-	void show(){
+	public void show(){
 		noStroke();
-		color c = color(255);
+		int c = color(255);
 		fill(c);
 		ellipse((float)x,(float)y,25,25);
 	}
-	void move(){
+	public void move(){
 		
 		x+= Math.cos(dTheta)*dSpeed;
 		y+= Math.sin(dTheta)*dSpeed;
 	}
 }//
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
